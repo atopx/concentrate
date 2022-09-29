782c246e9ae09adb9bf8f17aeec9b3f546796d5f
atopx <3940422@qq.com>
2022-09-29 20:27:14 +0800

    init project code

---
+++ src/components/Greet.vue
@@ -0,0 +1,21 @@
+<script setup>
+import { ref } from "vue";
+import { invoke } from "@tauri-apps/api/tauri";
+
+const greetMsg = ref("");
+const timestamp = ref("");
+
+async function greet() {
+  // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
+  greetMsg.value = await invoke("greet", { timestamp: timestamp.value });
+}
+</script>
+
+<template>
+  <div class="card">
+    <input id="greet-input" v-model="timestamp" placeholder="timestamp" />
+    <button type="button" @click="greet()">Greet</button>
+  </div>
+
+  <p>{{ greetMsg }}</p>
+</template>
